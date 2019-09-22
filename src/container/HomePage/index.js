57f140884aa44eb5e952db0d64ab4d6e56a169fa
Raja <rajamohamed701@gmail.com>
2019-09-22 20:25:38 +0530

    Home page changes

--- src/container/HomePage/index.js
+++ src/container/HomePage/index.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import '../styless.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 
 class HomePage extends Component {
   render() {
     return (
       <div>
+        {/* <CssBaseline /> */}
+      
         <div className="buttonStyle">
-          <input
-            type="button"
-            value="click"
-            onClick={() => this.props.history.push('/table')}
-          />
+        {/* <Container maxWidth="sm">
+        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '50vh' }} />
+      </Container> */}
+        <Button variant="contained" color="secondary"  onClick={() => this.props.history.push('/table')} >
+        Go to user page
+      </Button>
+          
         </div>
         <Particles
           params={{
